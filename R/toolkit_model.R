1f20786c3f8d531763007c6913fb6e0dd9025dbf
rosseji <rossejireland@gmail.com>
2016-10-28 08:41:27 +1100

    annotation edit

--- R/toolkit_model.R
+++ R/toolkit_model.R
@@ -2,4 +2,41 @@
 
 # Modelling toolkit
 
-# functions to write to tables the new rate, threshold, age, cap, etc.
+# functions to write to table the new rate, threshold, age, cap, etc.
+
+# Roxygen template
+
+#' Age Pension Master Function
+#'
+#' (Jackson to complete...)
+#'
+#' @name ysi_age_pension
+#' @param df Argument is intended to be a survey with the Tax_Matrix set, as a data frame
+#' @return A data frame
+#' @export
+
+# marginal_tax_model_add <- function(model_name = NULL, thresholds_count = NULL, thresholds_list = NULL, rates_list = NULL) {
+#
+#
+#
+#   temp_tbl <- income_tax_rates_tbl
+#
+#   for (n in thresholds_count) {
+#
+#     if (thresholds_list = NULL | thresholds_count = NULL |thresholds_list = NULL |rates_list = NULL )
+#       stop("At least one variable is not ")
+#     if (!is.character(model_name))
+#       stop("model_name is not a string")
+#     if (!is.numeric(thresholds_count))
+#       stop("set is not a number")
+#     if (thresholds_list = NULL)
+#       stop("set is not in the master... ")
+#     if (xwave == F & !wave_n %in% c(1:14))
+#       stop("wave_n is not in correct form")
+#
+#
+#   }
+#
+#
+# }
+
